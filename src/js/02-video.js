2057ec7637369acec5c946ab2deea3bca0fa573c
Evgenii Horokhov <gorohovevgeniyvalerievich@gmail.com>
2022-07-24 13:48:29 +0300

    add-two-task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_KEY = 'videoplayer-current-time';
+
+    player.on('timeupdate', Throttle(onPlay, 1000));
+
+    function onPlay(e) {
+        localStorage.setItem(LOCAL_KEY, e.seconds);
+    }
+
+setCurrentTime();
+
+function setCurrentTime() {
+    const savedTime = localStorage.getItem(LOCAL_KEY);
+    if (savedTime) {
+        player.setCurrentTime(savedTime);
+    }
+}

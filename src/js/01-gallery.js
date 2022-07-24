b971ee9df56d6e80e32a00e36247ab16dc94b11b
Evgenii Horokhov <gorohovevgeniyvalerievich@gmail.com>
2022-07-24 16:01:06 +0300

    prettier-format

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Change code below this line
 
 console.log(galleryItems);
 
-const markup = galleryItems.reduce((acc, {original, preview, description}) => {
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) => {
     return (
-        acc + 
-    `<a class="gallery__link" href="${original}">
+      acc +
+      `<a class="gallery__link" href="${original}">
         <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
           </a>`
     );
-},
- ""
+  },
+  ''
 );
 const galleryContainer = document.querySelector('.gallery');
 
 galleryContainer.insertAdjacentHTML('beforeend', markup);
 
 new SimpleLightbox('.gallery a');
-
